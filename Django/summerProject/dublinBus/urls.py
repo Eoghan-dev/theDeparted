80b6abb28ad9a0b4f3392294590054cff30d3a65
Finnian Rogers <finnianrogers@gmail.com>
2021-06-21 13:08:44 +0100

    added basic view to call scraper and changed home page to show recent entries to the table

--- Django/summerProject/dublinBus/urls.py
+++ Django/summerProject/dublinBus/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('scrapeCW', views.scrapeCW, name='scrapeCW')
 ]
--- Django/summerProject/dublinBus/views.py
+++ Django/summerProject/dublinBus/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from django.template import loader
+from .models import CurrentWeather
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    CurrentWeatherList = CurrentWeather.objects.order_by('entry_id')[:5]
+    template = loader.get_template('dublinBus/index.html')
+    context = {
+        'CurrentWeatherList': CurrentWeatherList,
+    }
+    return HttpResponse(template.render(context, request))
+
+def scrapeCW(request):
+    """View to call our scrape method in the CurrentWeather class."""
+    CurrentWeather.scrape()
+    return HttpResponse("Finished scraping, results saved to database!")

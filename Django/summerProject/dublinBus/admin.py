b708a09558224bbc635efbbfadce7171e12ad2a7
Finnian Rogers <finnianrogers@gmail.com>
2021-06-30 21:26:53 +0100

    registered bus_stops as model in admin page

--- Django/summerProject/dublinBus/admin.py
+++ Django/summerProject/dublinBus/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from dublinBus.models import CurrentWeather, CurrentBus
+from dublinBus.models import CurrentWeather, CurrenBus, bus_stops
 
 admin.site.register(CurrentWeather)
 admin.site.register(CurrentBus)
+admin.site.register(bus_stops)

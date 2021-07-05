61676011f4b4166910fff4d19209f7067028ed15
Finnian Rogers <finnianrogers@gmail.com>
2021-07-05 15:14:22 +0100

    Project structure change - added base.html file and made index page inherit from it.

--- Django/summerProject/docker_stuff.sh
+++ Django/summerProject/docker_stuff.sh
@@ -14,4 +14,4 @@ docker build --tag django_project:latest .
 # Create and run new container based on the updated image
 echo "create and run new docker container (also called 'django_project')"
 docker run --name django_project -d -it -p 8000:8000 django_project:latest
-echo "Finished!, django project should now be running in docker container"
+echo "Finished!, django project should now be running in docker container, you can stop it any time with 'docker stop django_project'"

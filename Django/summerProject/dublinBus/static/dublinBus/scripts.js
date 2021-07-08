97af07812eb38a7daa1b3b98a3a2c909e18604d3
Finnian Rogers <finnianrogers@gmail.com>
2021-07-08 17:16:42 +0100

    removed unnecessary parameter for showCertainMarkers function

--- Django/summerProject/dublinBus/static/dublinBus/scripts.js
+++ Django/summerProject/dublinBus/static/dublinBus/scripts.js
@@ -23,7 +23,7 @@ function scrapeCB() {
     })
 }
 
-function showCertainMarkers(allMarkers, visibleMarkers, map) {
+function showCertainMarkers(allMarkers, visibleMarkers) {
     // Function that takes an array of all the markers on the map, an array of the markers we want to make visible and
     // the google map object. The function makes only 'visibleMarkers' visible and hides all other markers
 

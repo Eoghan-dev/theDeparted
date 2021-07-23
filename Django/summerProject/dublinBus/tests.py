7369febe60bdebcc2380618e361f66e3393ab5dd
Kingstr3 <ross.kingston@ucdconnect.ie>
2021-07-23 08:19:12 +0100

    Created a new branch for testing purposes

--- Django/summerProject/dublinBus/tests.py
+++ Django/summerProject/dublinBus/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
 
+# pages/tests.py
+from django.http import HttpRequest
+from django.test import SimpleTestCase
+from django.urls import reverse
+
+from . import views
+
+
+class HomePageTests(SimpleTestCase):
+
+    def test_home_page_status_code(self):
+        response = self.client.get('/')
+        self.assertEquals(response.status_code, 200)
+
+    def test_view_url_by_name(self):
+        response = self.client.get(reverse('index'))
+        self.assertEquals(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse('index'))
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'dublinBus/index.html')
+
 # Create your tests here.

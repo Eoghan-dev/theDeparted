0079c012dd27e042cbea009be4bdf7b58ea78d1c
Shane <dunnser97@gmail.com>
2021-07-26 06:17:23 +0100

    -commented out redundant fuctions for this branch

--- Django/summerProject/dublinBus/static/dublinBus/map.js
+++ Django/summerProject/dublinBus/static/dublinBus/map.js
@@ -5,6 +5,14 @@ async function initMap() {
     // This is useful for things such as querying our db to get bus stop co-ordinates so that we can ensure that we have
     // these co-ordinates before trying to fill the map with markers based around them.
 
+    // Fill the drop down selector of routes before continuing
+    let routes = await fetch('/get_routes').then(res => {
+        return res.json()
+    }).then(data => {
+        console.log("all routes:", data)
+        return data
+    });
+
     // load map
     const map = new google.maps.Map(document.getElementById("map"), {
         zoom: 13,
@@ -12,7 +20,6 @@ async function initMap() {
     });
     var directionsService = new google.maps.DirectionsService();
     var directionsRenderer = new google.maps.DirectionsRenderer();
-    // directionsRenderer.setPanel(document.getElementById("sidebar")); makes and sets the sidebard
     directionsRenderer.setMap(map);
 
     // Make request to get json object of all dublin bus stops
@@ -22,16 +29,37 @@ async function initMap() {
     }).then(data => {
         console.log("bus stop data:", data)
         return data
-    })
+    });
     // Declare an empty array where we will keep all of our markers for each stop
     const markers_array = [];
+
     // This is what we can call from our html to load our directions
-    const onChangeHandler = function () {
-        // displayRoute(directionsService, directionsRenderer, markers_cluster, map); commented out for prototype demo
-        displayRoute(directionsService, directionsRenderer, markers_array, "56A");
+    const displaySelectedRoute = function () {
+        // The first index of the array returned by getElementsByName for our datalist with always be the selected route by the user
+        let selectedRoute = document.getElementsByName('routes_num')[0];
+        let routeNumberAndDir = selectedRoute.value;
+        console.log("Selected route was: ", routeNumberAndDir)
+        displayRoute(directionsService, directionsRenderer, markers_array, routeNumberAndDir)
     };
+    const displaySelectedStop = function () {
+        // The first index of the array returned by getElementsByName for our datalist with always be the selected stop by the user
+        let selectedStop = document.getElementsByName('stops_num')[0];
+        let stopNum = selectedStop.value;
+      //  let stopVal = selectedStop.value;
+        // The data-value attribute of the datalist can't be accessed directly so we need to use the following to get it
+        // let stopNum = document.querySelector("#stops" + " option[value='" + selectedStop + "']").dataset.value;
+
+        console.log("Selected stop was: ", stopNum)
+        displayStop(markers_array, stopNum)
+    };
+    // Call the function so the data is loaded in the search bar ready to autocomplete before it's clicked
+    //loadStopsSearch(bus_stop_data);
+    // Call our function to load the route data into the autocomplete search bar here so it's ready to go once the user clicks it
+    //loadRoutesSearch(routes);
     // Add an event listener to a button so we can call the above function which will then load our directions
-    document.getElementById("get_directions").addEventListener("click", onChangeHandler);
+    //document.getElementById("get_directions").addEventListener("click", displaySelectedRoute);
+    //document.getElementById('routes_num').addEventListener('change', displaySelectedRoute);
+    //document.getElementById('stops_num').addEventListener('change', displaySelectedStop);
 
     // Loop through our json object making a marker for each station and placing that marker on the map/saving it to an array
     for (let key in bus_stop_data) {
@@ -39,15 +67,25 @@ async function initMap() {
         // Create info window for each station before creating a marker
         // Save the routes serving this station in a new array by taking the first index from each entry in station.routes
         let station_routes = []
-         station.routes.forEach(route => {
-            station_routes.push(route[0]);
+        // Extract the stop number as the second word from station.stop_num
+        let station_number_arr = station.stop_num.split(" ");
+        let station_number = parseInt(station_number_arr[2]);
+
+        station.routes.forEach(route => {
+            // Make a combined string of the first index of each entry in routes (route number) and the second (direction of route)
+            let routes_string = route[0] + ": " + route[1];
+            station_routes.push(routes_string);
         })
-            // Create content of window
+
+        // Create content of window
         let window_content = `<h1>Station Name: ${station.stop_name}</h1>` +
             `<ul>` +
-                `<li>Station Number: ${station.stop_num} </li>` +
-                `<li>Routes served by station: ${station_routes.toString()}` +
-            `</ul>`;
+            `<li>Station Number: ${station_number} </li>` +
+            `<li>Routes served by station:<ul>`;
+        for (let route of station_routes) {
+            window_content += `<li>${route}</li>`;
+        }
+        window_content += "</ul></ul>";
         // Create info window object
         let current_info_window = new google.maps.InfoWindow({
             content: window_content,
@@ -59,11 +97,11 @@ async function initMap() {
             position: current_marker_location,
             map: map,
             name: station.stop_name,
-            number: parseInt(station.stop_num),
+            number: station_number,
             infowindow: current_info_window,
             // Icon taken from http://kml4earth.appspot.com/icons.html
             // icon: "http://maps.google.com/mapfiles/kml/shapes/bus.png", this is the hideous icon
-            routes: station.routes,
+            routes: station_routes,
         });
         // Add an on-click event for each marker to open the associated info window
         current_marker.addListener("click", () => {
@@ -80,13 +118,36 @@ async function initMap() {
         //Now add each created marker to our array of markers to keep track of them
         markers_array.push(current_marker);
         // Also add each marker to our map
+        current_marker.setVisible(false)
         current_marker.setMap(map);
     }
+    // We can initialise our class to handle directions here once we've declared all the arguments and filled the markers array
+    //new AutocompleteDirectionsHandler(map, markers_array, directionsService, directionsRenderer);
 
-    // Add a marker clusterer to group all the markers together using the Marker Clusterer Plus library https://github.com/googlemaps/js-markerclustererplus
-    // let markers_cluster = new MarkerClusterer(map, markers_array, {ignoreHidden: true}, {
-    //   imagePath:
-    //     "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
-    // });
+    // Get user geolocation (adapted from https://developers.google.com/maps/documentation/javascript/examples/map-geolocation)
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+                const pos = {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude,
+                };
+                const user_marker = new google.maps.Marker({
+                    position: pos,
+                    map: map,
+                    name: "user location",
+                    // Icon taken from http://kml4earth.appspot.com/icons.html
+                    icon: "http://maps.google.com/mapfiles/kml/shapes/man.png",
+                });
+            },
+            () => {
+                handleLocationError(true, map);
+            }
+        );
+    } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, map);
+    }
+
     console.log("Markers array:", markers_array)
 }

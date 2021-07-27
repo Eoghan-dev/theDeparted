3690ae6694455e17afb04a7e2e03231a4a6b6973
Kingstr3 <ross.kingston@ucdconnect.ie>
2021-07-27 09:08:34 +0100

    Added new tests to verify journeys view

--- Django/summerProject/dublinBus/tests.py
+++ Django/summerProject/dublinBus/tests.py
@@ -8,19 +8,32 @@ from django.urls import reverse
 from . import views
 
 
-class HomePageTests(SimpleTestCase):
+class AppPageTests(SimpleTestCase):
 
-    def test_home_page_status_code(self):
+    def test_index_page_status_code(self):
         response = self.client.get('/')
         self.assertEquals(response.status_code, 200)
 
-    def test_view_url_by_name(self):
+    def test_journey_page_status_code(self):
+        response = self.client.get('/journey')
+        self.assertEquals(response.status_code, 200)
+
+    def test_index_view_url_by_name(self):
         response = self.client.get(reverse('index'))
         self.assertEquals(response.status_code, 200)
 
-    def test_view_uses_correct_template(self):
+    def test_journey_view_url_by_name(self):
+        response = self.client.get(reverse('journey'))
+        self.assertEquals(response.status_code, 200)
+
+    def test_index_view_uses_correct_template(self):
         response = self.client.get(reverse('index'))
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'dublinBus/index.html')
+
+    def test_journey_view_uses_correct_template(self):
+        response = self.client.get(reverse('journey'))
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'dublinBus/journey.html')
 
 # Create your tests here.

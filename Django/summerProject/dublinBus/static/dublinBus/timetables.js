605b24389be75fd6728013465f144e60f016beb4
Shane <dunnser97@gmail.com>
2021-08-16 09:13:26 +0100

    -Changed to grid system format rather than a table

--- Django/summerProject/dublinBus/static/dublinBus/timetables.js
+++ Django/summerProject/dublinBus/static/dublinBus/timetables.js
@@ -2,26 +2,22 @@ fetch("/timetable_route").then(response => {
     return response.json();
 }).then(data => {
     console.log(data)
-    results_output = "<table id='resultsTable'>"
-    results_output += "<tr> <th> Station Name </th> <tr>"
+    results_output = "<div id='resultsTable'>"
+    results_output += "<div class='row'><div class='col-xs-6 offset-xs-3 col-8 offset-2 col-md-4 offset-md-4 py-2 my-1 text-center bg-warning'> Station Name </div></div>";
+    results_output += "<div class='row'>";
     for (var key in data){
         var arrayLength = data[key]["direction"].length;
         for (var dir = 0; dir < arrayLength; dir++) {
             direction = data[key]["direction"][dir][0]
-            results_output += "<tr class='stname'>";
             var direction_url = direction.replace("'","%27");
             direction_url = direction_url.replace(" ","%20");
-            results_output += "<th class='stname'><a href='/timetable/" + key + ":"+direction_url+"'>" + key + ":"+direction + "</a></th>";
-            results_output += "</tr>";
+            results_output += "<span class=\"border border-dark bg-warning border-5><div class='stname col-md-4 offset-md-4 col-xs-6 offset-xs-3 col-8 offset-2 py-1 my-1 text-center'><a href='/timetable/" + key + ":"+direction_url+"'>" + key + ":"+direction + "</a></div></span>";
+
             }
     }
-
-
-    //Loop through all stations and add a table row for each station.
-    //Table ends.
-    results_output += "</table>";
+    results_output += "</div>";
     document.getElementById("searchresults").innerHTML = results_output;
-
+    results_output += "</div>"
 }).catch(err => {
     document.getElementById("searchresults").innerHTML = "<h1>Error! The stations cannot be loaded at this time</h1>";
 })
